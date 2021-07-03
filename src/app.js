aaeb73e2c5846bea039714fd8a4512d533873828
Avinash Malhotra <avimalhotra505@gmail.com>
2021-07-03 15:53:05 +0530

    Backup Class code

---
+++ src/app.js
@@ -0,0 +1,13 @@
+//console.log(process.version);
+
+let ext=require('./ext');                   // ext module
+const os=require("os");                     // build in 
+const colors=require('colors');             // 3rd part from npmjs.org
+
+//console.log(id, pass);
+
+//console.log("RED".red);
+//console.log("GREEN".green);
+
+//console.log(ext);
+//console.log(ext.x, ext.y);
---
+++ src/ext.js
@@ -0,0 +1,9 @@
+//global.id=1234;
+//global.pass='ab1234';
+
+
+let ip=1234;
+//module.exports=ip;                            // single export
+
+exports.x=ip;                               // multiple export
+exports.y=3.14;                               // multiple export
